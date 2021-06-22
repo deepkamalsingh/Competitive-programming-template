0601495bd11377e6726bcaaee7d13baf07364f22
deepkamal <deepkamalsinghjassal@gmail.com>
2021-06-22 11:42:18 +0530

    corrected indentation of rmq

--- Graph and tree Algos/lca (rmq) .cpp
+++ Graph and tree Algos/lca (rmq) .cpp
@@ -1,99 +1,99 @@
-/**
- * Description: 1D range minimum query. Can also do queries 
- 	* for any associative operation in $O(1)$ with D\&C
- * Source: KACTL
- * Verification: 
-	* https://cses.fi/problemset/stats/1647/
-	* http://wcipeg.com/problem/ioi1223
-	* https://pastebin.com/ChpniVZL
- * Memory: O(N\log N)
- * Time: O(1)
- * Zero based indexing 
- * gives ans for [l, r]
- */
-template<class T> struct RMQ { // floor(log_2(x))
-	int level(int x) { 
-    return 31 - __builtin_clz(x); 
-  }
-   
-	vector<T> v ; 
-  vector<vector<int>> jmp;
-	int comb(int a, int b) { // index of min
-		return v[a] == v[b] ? min(a,b) : (v[a] < v[b] ? a : b) ; 
-  }
-   
-	void init(const vector<T>& _v) {
+/*
+	description :- static_range_minimum_query, uses divide and conquer
+	Time :- O(1) query with O(N log N) preprocessing.
+	Sources :- https://github.com/bqi343/USACO/blob/master/Implementations/content/data-structures/Static%20Range%20Queries%20(9.1)/RMQ%20(9.1).h
+	verification :- https://judge.yosupo.jp/submission/51130
+*/
+
+template<class T> struct static_range_minimum_query {
+	
+	int level(int x) {
+		return 31 - __builtin_clz(x);
+	}
+	
+	vector<T> v;
+	vector<vector<int>> jmp;
+	
+	int comb(int a, int b){
+		return v[a] == v[b] ? min(a,b) : (v[a] < v[b] ? a : b);
+	}
+	
+	void init(const vector<T> & _v){
 		v = _v; 
-		jmp = { vector<int> (int(v.size())) }; 
-    iota(begin(jmp[0]), end(jmp[0]), 0) ;
-		for (int j = 1; (1<<j) <= int(v.size()); ++j) {
-			jmp.push_back(vector<int>(int((v).size()) - (1<<j) + 1));
+		jmp = {vector<int> (int(_v.size()))};
+		iota(begin(jmp[0]),end(jmp[0]),0);
+		for(int j = 1; (1 << j) <= int(_v.size()); ++j){
+			jmp.push_back(vector<int> (int(_v.size()) - (1 << j) + 1));
 			for(int i = 0; i < int(jmp[j].size()); ++i){
-        jmp[j][i] = comb(jmp[j-1][i], jmp[j-1][i + (1<<(j-1))]) ;
-      } 
+				jmp[j][i] = comb(jmp[j-1][i], jmp[j-1][i + (1 << (j - 1))]);
+			}
+		}	
+	}
+	
+	int index(int l, int r){
+		assert(l <= r);
+		int d = level(r - l + 1);
+		return comb(jmp[d][l], jmp[d][r - (1 << d) + 1]);
+	}
+	
+	T query(int l, int r){
+		return v[index(l,r)];
+	}
+	
+};
+
+
+/*
+ * Description: Euler Tour LCA.
+	* The root points to itself.
+ * Time: O(N log N) build, O(1) LCA
+ * Source: https://github.com/bqi343/USACO/blob/master/Implementations/content/graphs%20(12)/Trees%20(10)/LCArmq%20(10.2).h
+ * Verification: https://judge.yosupo.jp/submission/51135
+*/
+ 
+ 
+struct lowest_common_ancestor {
+	int N;
+	vector<vector<int>> adj;
+	vector<int> depth, pos, par;
+	vector<pair<int,int>> tmp;
+	static_range_minimum_query<pair<int,int>> rmq;
+	
+	void init(int _N){
+		N = _N;
+		adj.resize(N);
+		depth = pos = par = vector<int> (N);
+	}
+	
+	void add_edge(int u,int v){
+		adj[u].push_back(v);
+		adj[v].push_back(u);
+	}
+	
+	void dfs(int x){
+		pos[x] = int(tmp.size());
+		tmp.emplace_back(depth[x], x);
+		for(auto y : adj[x]) if(y != par[x]){
+			depth[y] = depth[par[y] = x] + 1;
+			dfs(y);
+			tmp.emplace_back(depth[x],x);
 		}
 	}
 	
-	int index(int l, int r) { // get index of min element
-		assert(l <= r); 
-    int d = level(r-l+1);
-		return comb(jmp[d][l], jmp[d][r-(1<<d)+1]); 
-  }
-  
-	T query(int l, int r) { 
-    return v[index(l,r)]; 
-  }
-};
-/**
- * Description: Euler Tour LCA.
-	* pairwise LCAs.
-	* The root points to itself.
-	* include RMQ for this
- * Time: O(N\log N) build, O(1) LCA, O(|S|\log |S|) compress
- * Source: USACO, Simon Lindholm (KACTL)
- * Verification: USACO Debug the Bugs
- 	* https: ??
- */
-struct LCA {
-	int N; vector<vector<int>> adj;
-	vector<int> depth, pos, par; 
-	vector<pair<int,int>> tmp; 
-  RMQ<pair<int,int>> rmq;
-  
-	void init(int _N) { 
-    N = _N ; 
-    adj.resize(N) ; 
-		depth = pos = par = vector<int> (N) ; 
-  }
-  
-	void ae(int x, int y) { 
-    adj[x].push_back(y), adj[y].push_back(x); 
-  }
-  
-	void dfs(int x) {
-		pos[x] = int(tmp.size()); 
-    tmp.emplace_back(depth[x], x) ; 
-		for(auto y:adj[x]) if (y != par[x]) {
-			depth[y] = depth[par[y] = x] + 1;
-      dfs(y);
-			tmp.emplace_back(depth[x],x); 
-    }
+	void gen(int R = 0){
+		par[R] = R;
+		dfs(R);
+		rmq.init(tmp);
 	}
 	
-	void gen(int R = 0) { 
-    par[R] = R; 
-    dfs(R); 
-    rmq.init(tmp); 
-  }
-  
 	int lca(int u, int v){
-		u = pos[u], v = pos[v]; 
-    if (u > v) swap(u,v);
-		return rmq.query(u,v).second ; 
-  }
-  
-	int dist(int u, int v) {
-		return depth[u] + depth[v] - 2 * depth[lca(u,v)]; 
-  }
-};
+		u = pos[u], v = pos[v];
+		if(u > v) swap(u,v);
+		return rmq.query(u,v).second;
+	}	
+	
+	int dist(int u, int v){
+		return depth[u] + depth[v] - 2 * depth[lca(u,v)];
+	}
 
+};
--- Other Range Queries/rmq.cpp
+++ Other Range Queries/rmq.cpp
@@ -1,40 +1,45 @@
-/**
- * Description: 1D range minimum query. Can also do queries 
- 	* for any associative operation in $O(1)$ with D\&C
- * Source: KACTL, BENQ
- * Verification: 
-	* https://cses.fi/problemset/result/1755689/
- * Memory: O(N\log N)
- * Time: O(1)
- * Zero based indexing 
- * gives ans for [l, r]
- */
-template<class T> struct RMQ { // floor(log_2(x))
-	int level(int x) { 
-    return 31 - __builtin_clz(x); 
-  } 
-	vector<T> v; vector<vector<int> > jmp;
-	int comb(int a, int b) { // index of min
-		return v[a]==v[b] ? min(a,b) : (v[a]<v[b] ? a : b) ; 
-  } 
-	void init(const vector<T> & _v) {
-		v = _v; jmp = { vector<int> ((int)((v).size())) } ; 
-    iota(begin(jmp[0]), end(jmp[0]), 0) ;
-		for (int j = 1; 1<<j <= (int)((v).size()); ++j) {
-			jmp.push_back(vector<int>((int)((v).size())-(1<<j)+1));
-			for(int i=0; i < (int)((jmp[j]).size()) ; ++i){
-        jmp[j][i] = comb(jmp[j-1][i], jmp[j-1][i+(1<<(j-1))]) ;
-      } 
-		}
+/*
+	description :- static_range_minimum_query, uses divide and conquer
+	Time :- O(1) query with O(N log N) preprocessing.
+	Sources :- https://github.com/bqi343/USACO/blob/master/Implementations/content/data-structures/Static%20Range%20Queries%20(9.1)/RMQ%20(9.1).h
+	verification :- https://judge.yosupo.jp/submission/51130
+*/
+
+template<class T> struct static_range_minimum_query {
+	
+	int level(int x) {
+		return 31 - __builtin_clz(x);
 	}
-	int index(int l, int r) { // get index of min element
-		assert(l <= r); 
-    int d = level(r-l+1);
-		return comb(jmp[d][l], jmp[d][r-(1<<d)+1]); 
-  }
-	T query(int l, int r) { 
-    return v[index(l,r)]; 
-  }
+	
+	vector<T> v;
+	vector<vector<int>> jmp;
+	
+	int comb(int a, int b){
+		return v[a] == v[b] ? min(a,b) : (v[a] < v[b] ? a : b);
+	}
+	
+	void init(const vector<T> & _v){
+		v = _v; 
+		jmp = {vector<int> (int(_v.size()))};
+		iota(begin(jmp[0]),end(jmp[0]),0);
+		for(int j = 1; (1 << j) <= int(_v.size()); ++j){
+			jmp.push_back(vector<int> (int(_v.size()) - (1 << j) + 1));
+			for(int i = 0; i < int(jmp[j].size()); ++i){
+				jmp[j][i] = comb(jmp[j-1][i], jmp[j-1][i + (1 << (j - 1))]);
+			}
+		}	
+	}
+	
+	int index(int l, int r){
+		assert(l <= r);
+		int d = level(r - l + 1);
+		return comb(jmp[d][l], jmp[d][r - (1 << d) + 1]);
+	}
+	
+	T query(int l, int r){
+		return v[index(l,r)];
+	}
+	
 };
 
 
@@ -55,42 +60,3 @@ template<class T> struct RMQ { // floor(log_2(x))
 
 
 
-
-
-
-
-const int MAXN=305000;
-const int MAXLOG=20;
-
-int n,a[MAXN];
-
-int table[MAXLOG][MAXN][2],logs[MAXN];
-
-void computeLogs(){
-	logs[1]=0;
-	f(i,2,n+1)	logs[i]=logs[i/2]+1;	
-}
-
-void buildSparseTable(){
-  	f(i,0,logs[n]+1){
-    	int curLen=(1LL<<i);
-    	f(j,0,n-curLen+1){
-      		if(curLen == 1) {
-        		table[i][j][0]=table[i][j][1]=a[j];
-      		}else{
-        		table[i][j][0]=min( table[i-1][j][0] , table[i-1][j + (curLen/2)][0] );
-        		table[i][j][1]=max( table[i-1][j][1] , table[i-1][j + (curLen/2)][1] );
-      		}
-    	}
-  	}
-}
-
-int getMin(int l,int r){
-	int p=logs[r-l+1];
-	return min(table[p][l][0],table[p][r-(1LL<<p)+1][0]);
-}
-
-int getMax(int l,int r){
-	int p=logs[r-l+1];
-	return max(table[p][l][1],table[p][r-(1LL<<p)+1][1]);
-}

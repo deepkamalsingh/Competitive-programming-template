976f073fe0261713674e4a993f826e6a21b77256
deepkamal <deepkamalsinghjassal@gmail.com>
2021-06-22 18:39:57 +0530

    spaces v/s tabs

--- Number Theory/modint.cpp
+++ Number Theory/modint.cpp
@@ -3,21 +3,21 @@
  * Source :- https://codeforces.com/contest/1342/submission/78409946
  * Verification:- https://open.kattis.com/problems/modulararithmetic
  */
- 
+
 template<const int &MOD>
 struct _m_int {
   int val;
- 
+
   _m_int(int64_t v = 0) {
     if(v < 0) v = v % MOD + MOD;
     if(v >= MOD) v %= MOD;
     val = v;
   }
- 
+
   static int inv_mod(int a, int m = MOD) {
   	// https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm#Example
     int g = m, r = a, x = 0, y = 1;
- 
+
     while(r != 0) {
       int q = g / r;
       g %= r; swap(g, r);
@@ -28,19 +28,19 @@ struct _m_int {
  	// wont work if we use #define int int64_t
   // explicit operator int() const { return val; }
   explicit operator int64_t() const { return val; }
- 
+
   _m_int& operator+=(const _m_int &other) {
     val -= MOD - other.val;
     if(val < 0) val += MOD;
     return *this;
   }
- 
+
   _m_int& operator-=(const _m_int &other) {
   	val -= other.val;
     if(val < 0) val += MOD;
     return *this;
   }
- 
+
   static unsigned fast_mod(uint64_t x, unsigned m = MOD) {
 #if !defined(_WIN32) || defined(_WIN64)
     return x % m;
@@ -54,46 +54,46 @@ struct _m_int {
       : "d" (x_high), "a" (x_low), "r" (m));
     return rem;
   }
- 
+
   _m_int& operator*=(const _m_int &other) {
     val = fast_mod((uint64_t) val * other.val);
     return *this;
   }
- 
+
   _m_int& operator/=(const _m_int &other) {
     return *this *= other.inv();
   }
- 
+
   friend _m_int operator+(const _m_int &a, const _m_int &b) { return _m_int(a) += b; }
   friend _m_int operator-(const _m_int &a, const _m_int &b) { return _m_int(a) -= b; }
   friend _m_int operator*(const _m_int &a, const _m_int &b) { return _m_int(a) *= b; }
   friend _m_int operator/(const _m_int &a, const _m_int &b) { return _m_int(a) /= b; }
- 
+
   _m_int& operator++() {
     val = val == MOD - 1 ? 0 : val + 1;
     return *this;
   }
- 
+
   _m_int& operator--() {
     val = val == 0 ? MOD - 1 : val - 1;
     return *this;
   }
- 	
+
  	// wont work if we use #define int int64_t
   // _m_int operator++(int) { _m_int before = *this; ++*this; return before; }
   // _m_int operator--(int) { _m_int before = *this; --*this; return before; }
- 
+
   _m_int operator-() const {
     return val == 0 ? 0 : MOD - val;
   }
- 
+
   bool operator==(const _m_int &other) const { return val == other.val; }
   bool operator!=(const _m_int &other) const { return val != other.val; }
- 
+
   _m_int inv() const {
     return inv_mod(val);
   }
- 
+
   _m_int pow(int64_t p) const {
   	if(p < 0)	return inv().pow(-p);
     _m_int a = *this, result = 1;
@@ -104,33 +104,33 @@ struct _m_int {
     }
     return result;
   }
- 
+
   friend ostream& operator<<(ostream &os, const _m_int &m) {
     return os << m.val;
   }
 };
- 
+
 extern const int MOD = int(1e9) + 7;
 using mint = _m_int<MOD>;
- 
+
 vector<mint> inv, factorial, inv_factorial, sub_factorial;
- 
+
 void prepare_factorials(int maximum) {
 	// Make sure MOD is prime, which is necessary for the inverse algorithm below.
   for(int p = 2; p * p <= MOD; p += p % 2 + 1){
     assert(MOD % p != 0);
   }
- 
+
   inv = {0, 1};
   factorial = inv_factorial = {1, 1};
  	sub_factorial = {1, 0};
- 	
+
   maximum = max(maximum, 1);
   inv.resize(maximum + 1);
   factorial.resize(maximum + 1);
   inv_factorial.resize(maximum + 1);
  	sub_factorial.resize(maximum + 1);
- 	
+
   for(int i = 2; i <= maximum; i++) {
     inv[i] = inv[MOD % i] * (MOD - MOD / i);
     factorial[i] = i * factorial[i - 1];
@@ -138,22 +138,22 @@ void prepare_factorials(int maximum) {
     sub_factorial[i] = i * sub_factorial[i-1] + ((i&1) ? -1 : 1);
   }
 }
- 
+
 mint choose(int n, int r) {
   if (r < 0 || r > n) return 0;
   return factorial[n] * inv_factorial[r] * inv_factorial[n - r];
 }
- 
+
 mint inv_choose(int n, int r) {
   assert(0 <= r && r <= n);
   return inv_factorial[n] * factorial[r] * factorial[n - r];
 }
- 
+
 mint permute(int n, int r) {
   if (r < 0 || r > n) return 0;
   return factorial[n] * inv_factorial[n - r];
 }
- 
+
 mint inv_permute(int n, int r) {
   assert(0 <= r && r <= n);
   return inv_factorial[n] * factorial[n - r];
@@ -162,7 +162,7 @@ mint inv_permute(int n, int r) {
 mint catalan(int n) {
 	return C(2*n, n) / (n+1);
 }
- 
+
 mint stirling_second(int n, int k) {
   mint answer = 0;
   for(int x = 0; x <= k; x++){
--- Segment tree/segment tree (bottom up).cpp
+++ Segment tree/segment tree (bottom up).cpp
@@ -7,27 +7,27 @@
 */
 
 template<class T> struct basic_segment_tree { // comb(ID,b) = b
-	const T ID =  0; 
-	T comb(T a, T b) {
-		return a + b; 
-	}
-	int n; vector<T> seg;
-	void init(int _n) { 
-		n = _n; seg.assign(2*n,ID); 
-	}
-	void pull(int p) { 
-		seg[p] = comb(seg[2*p], seg[2*p + 1]); 
-	}
-	void upd(int p, T val) { // update val at position p
-		seg[p += n] = val; 
-		for (p /= 2; p; p /= 2) pull(p); 
-	}
-	T query(int l, int r) { // query on interval [l, r]
-		T ra = ID, rb = ID;
-		for (l += n, r += n+1; l < r; l /= 2, r /= 2) {
-			if (l&1) ra = comb(ra,seg[l++]);
-			if (r&1) rb = comb(seg[--r],rb);
-		}
-		return comb(ra,rb);
-	}
+  const T ID =  0;
+  T comb(T a, T b) {
+    return a + b;
+  }
+  int n; vector<T> seg;
+  void init(int _n) {
+    n = _n; seg.assign(2*n,ID);
+  }
+  void pull(int p) {
+    seg[p] = comb(seg[2*p], seg[2*p + 1]);
+  }
+  void upd(int p, T val) { // update val at position p
+    seg[p += n] = val;
+    for (p /= 2; p; p /= 2) pull(p);
+  }
+  T query(int l, int r) { // query on interval [l, r]
+    T ra = ID, rb = ID;
+    for (l += n, r += n+1; l < r; l /= 2, r /= 2) {
+      if (l&1) ra = comb(ra,seg[l++]);
+      if (r&1) rb = comb(seg[--r],rb);
+    }
+    return comb(ra,rb);
+  }
 };
